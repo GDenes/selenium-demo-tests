362ce3b7e3bfbd289c0c8700368dd989899e7e7a
Kozma Norbert <norbert.kozma96@gmail.com>
2021-03-09 13:33:16 +0100

    Fix wait condition

--- src/main/java/com/selenium/demo/webshop/pageobjects/signin/SignInPage.java
+++ src/main/java/com/selenium/demo/webshop/pageobjects/signin/SignInPage.java
@@ -9,37 +9,39 @@ import com.selenium.demo.wait.WaitForElementToAppear;
 import com.selenium.demo.webshop.pageobjects.common.AbstractWebShopPage;
 
 public class SignInPage extends AbstractWebShopPage {
-	
+
 	@FindBy(css = ".page-heading")
 	private WebElement pageHead;
-	
+
 	@FindBy(css = "button#SubmitCreate > span")
 	private WebElement createAccountButton;
-	
+
 	@FindBy(css = "button#SubmitLogin > span")
 	private WebElement signInButton;
-	
+
 	@FindBy(css = "li.step_current > span")
 	private WebElement currentStep;
 
 	public SignInPage(WebDriver driver, BrowsersEnum browser) {
 		super(driver, browser);
 	}
-	
+
 	@Override
 	protected void isLoaded() throws Error {
 		super.isLoaded();
-		
-		new WaitForElementToAppear(driver).apply(pageHead);
-		new WaitForElementToAppear(driver).apply(createAccountButton);
-		new WaitForElementToAppear(driver).apply(signInButton);
-		new WaitForElementToAppear(driver).apply(currentStep);
+
+		final WaitForElementToAppear waitForElementToAppear = new WaitForElementToAppear(driver);
+
+		waitForElementToAppear.apply(pageHead);
+		waitForElementToAppear.apply(createAccountButton);
+		waitForElementToAppear.apply(currentStep);
+		waitForElementToAppear.apply(signInButton);
 	}
-	
+
 	public String getHeadText() {
 		return pageHead.getText();
 	}
-	
+
 	public String getCurrentStepText() {
 		return currentStep.getText();
 	}
