4177b70b3af53e275b8772fcfe9bef78dfc93412
Denes Gombis <gd9603@gmail.com>
2021-03-09 10:50:20 +0100

    Delete unused constans and variable

--- src/main/java/com/selenium/demo/testbase/TestBase.java
+++ src/main/java/com/selenium/demo/testbase/TestBase.java
@@ -1,5 +1,6 @@
 package com.selenium.demo.testbase;
 
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
@@ -8,13 +9,11 @@ import org.slf4j.LoggerFactory;
 
 import com.selenium.demo.testbase.drivers.DriverFactory;
 
-
 public abstract class TestBase {
 	
 	protected static final Logger logger = LoggerFactory.getLogger(TestBase.class);
 
 	protected static final BrowsersEnum browser = BrowsersEnum.CHROME_HEADLESS;
-	protected static final String PIPELINE_HISTORY_PAGE = "http://localhost"; 
 	private static WebDriver driver = new DriverFactory().createDriver(browser);
 	
 	@BeforeEach
