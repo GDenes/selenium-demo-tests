aeecaa7d74b04a586e9dfc2c5dbd9103b98d50a3
Denes Gombis <gd9603@gmail.com>
2021-03-18 14:29:34 +0100

    Fix Remote test run, firefox, edge

--- src/main/java/com/selenium/demo/testbase/driver/config/EdgeConfig.java
+++ src/main/java/com/selenium/demo/testbase/driver/config/EdgeConfig.java
@@ -1,17 +1,16 @@
 package com.selenium.demo.testbase.driver.config;
 
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.remote.CapabilityType;
+import com.microsoft.edge.seleniumtools.EdgeOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EdgeConfig extends GenericConfig {
 
-    protected static final Logger logger = LoggerFactory.getLogger(ChromeConfig.class);
+    protected static final Logger logger = LoggerFactory.getLogger(EdgeConfig.class);
 
     public EdgeOptions getOptions() {
         final EdgeOptions options = new EdgeOptions();
-        options.setCapability("UseChromium", true);
+        options.addArguments("disable-gpu");
 
         return options;
     }
--- src/main/java/com/selenium/demo/testbase/drivers/DriverFactory.java
+++ src/main/java/com/selenium/demo/testbase/drivers/DriverFactory.java
@@ -6,35 +6,41 @@ import com.selenium.demo.testbase.BrowsersEnum;
 
 public class DriverFactory {
 
-	public WebDriver createDriver(final BrowsersEnum browser) {
-		switch (browser) {
-		case CHROME:
-			return new ChromeFactory().getDriver();
-		case CHROME_HEADLESS:
-			return new ChromeHeadlessFactory().getDriver();
-		case FIREFOX:
-			return new FirefoxFactory().getDriver();
-		case FIREFOX_HEADLESS:
-			return new FirefoxHeadLessFactory().getDriver();
-		case EDGE:
-			return new EdgeFactory().getDriver();
-		default:
-			throw new UnsupportedOperationException("Browser type is not yet supported");
-		}
-	}
+    public WebDriver createDriver(final BrowsersEnum browser) {
+        switch (browser) {
+            case CHROME:
+                return new ChromeFactory().getDriver();
+            case CHROME_HEADLESS:
+                return new ChromeHeadlessFactory().getDriver();
+            case FIREFOX:
+                return new FirefoxFactory().getDriver();
+            case FIREFOX_HEADLESS:
+                return new FirefoxHeadLessFactory().getDriver();
+            case EDGE:
+                return new EdgeFactory().getDriver();
+            case EDGE_HEADLESS:
+                return new EdgeHeadlessFactory().getDriver();
+            default:
+                throw new UnsupportedOperationException("Browser type is not yet supported");
+        }
+    }
 
-	public WebDriver createRemoteDriver(final BrowsersEnum browser, String remoteUrl) {
-		switch (browser) {
-			case CHROME:
-				return new ChromeFactory().getRemoteWebDriver(remoteUrl);
-			case CHROME_HEADLESS:
-				return new ChromeHeadlessFactory().getRemoteWebDriver(remoteUrl);
-			case FIREFOX:
-				return new FirefoxFactory().getRemoteWebDriver(remoteUrl);
-			case FIREFOX_HEADLESS:
-				return new FirefoxHeadLessFactory().getRemoteWebDriver(remoteUrl);
-			default:
-				throw new UnsupportedOperationException("Browser type is not yet supported");
-		}
-	}
+    public WebDriver createRemoteDriver(final BrowsersEnum browser, String remoteUrl) {
+        switch (browser) {
+            case CHROME:
+                return new ChromeFactory().getRemoteWebDriver(remoteUrl);
+            case CHROME_HEADLESS:
+                return new ChromeHeadlessFactory().getRemoteWebDriver(remoteUrl);
+            case FIREFOX:
+                return new FirefoxFactory().getRemoteWebDriver(remoteUrl);
+            case FIREFOX_HEADLESS:
+                return new FirefoxHeadLessFactory().getRemoteWebDriver(remoteUrl);
+            case EDGE:
+                return new EdgeFactory().getRemoteWebDriver(remoteUrl);
+            case EDGE_HEADLESS:
+                return new EdgeHeadlessFactory().getRemoteWebDriver(remoteUrl);
+            default:
+                throw new UnsupportedOperationException("Browser type is not yet supported");
+        }
+    }
 }
